cd70646fbb9b6f6105191fa14751b5fe3e24ef64
april <april.trostle1@gmail.com>
2024-12-28 12:58:56 -0500

    Keyboard setup and keyboard input begun

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 use std::collections::BTreeMap;
 use std::sync::LazyLock;
-use iced::{Element, Color, Padding};
+use iced::{Element, Color, Padding, Subscription, keyboard};
+use iced::font::{Font, Weight};
 use iced::widget::{Container, container, row, column, text, Row};
+use iced::keyboard::{Key, Modifiers, key::Named};
 
 pub static GREY: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0x88, 0x88, 0x88));
 pub static DARK_GREY: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0x44, 0x44, 0x44));
+pub static RED: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0xFF, 0x45, 0x00));
 pub static YELLOW: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0xFF, 0xCE, 0x1B));
 pub static GREEN: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0x04, 0x63, 0x07));
 pub static BLACK: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0x00, 0x00, 0x00));
 pub static WHITE: LazyLock<Color, fn() -> Color> = LazyLock::new(|| Color::from_rgb8(0xFF, 0xFF, 0xFF));
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+enum GameResult {
+    WIN,
+    LOSE,
+}
+
+#[derive(Debug, Clone)]
 enum Message {
     EnterText(char),
     DeleteText,
     Enter,
+    GameOver(GameResult),
 }
 
 #[derive(Copy, Clone)]
@@ -28,8 +38,8 @@ struct Entry {
 impl Default for Entry {
     fn default() -> Entry {
         Entry {
-            chars: ['A'; 5],
-            colors: [*GREY; 5],
+            chars: [' '; 5],
+            colors: [*DARK_GREY; 5],
             is_active: false,
             cursor: 0,
         }
@@ -52,32 +62,47 @@ impl Entry {
             .spacing(5)
             .into()
     }
-    fn update (&mut self, message: Message) {}
+    fn update(&mut self, message: Message) {}
+    fn grade(&mut self, grade_result: [(char, Color); 5]) {
+        self.colors = grade_result.map(|(_, color)| color);
+        // println!(
+        //     "Grade: {:?} {:?} {:?} {:?} {:?}",
+        //     self.colors[0], self.colors[1], self.colors[2], self.colors[3], self.colors[4]`
+        // );
+    }
 }
 
 // #[derive(Default)]
 struct EntrySet {
     entries: [Entry; 6],
     strings: [[char; 5]; 6],
+    active_entry: usize,
+    secret_word: String,
 }
 
 impl Default for EntrySet {
     fn default() -> EntrySet {
-        EntrySet {
+        let mut ans = EntrySet {
             entries: [Entry::default(); 6],
-            strings: [[' '; 5]; 6]
+            strings: [[' '; 5]; 6],
+            active_entry: 0,
+            secret_word: "tares".to_string(),
+        };
+        for i in 0..5 {
+            ans.entries[0].colors[i] = *GREY;
         }
+        ans
     }
 }
 
 impl EntrySet {
-    fn from_strings(strings: [[char; 5]; 6]) -> EntrySet {
-        let mut entries = [Entry::default(); 6];
-        for i in 0..strings.len() {
-            entries[i] = Entry::from_chars(strings[i]);
-        }
-        EntrySet {entries, strings}
-    }
+    // fn from_strings(strings: [[char; 5]; 6]) -> EntrySet {
+    //     let mut entries = [Entry::default(); 6];
+    //     for i in 0..strings.len() {
+    //         entries[i] = Entry::from_chars(strings[i]);
+    //     }
+    //     EntrySet {entries, strings, active_entry: 0}
+    // }
     fn view(&self) -> Element<'_, Message> {
         column![self.entries[0].view(),
         self.entries[1].view(),
@@ -86,7 +111,7 @@ impl EntrySet {
         self.entries[4].view(),
         self.entries[5].view()]
             .spacing(10)
-            .padding(Padding {top:100.0, right: 0.0, bottom: 0.0, left: 350.0})
+            .padding(Padding {top:20.0, right: 0.0, bottom: 0.0, left: 350.0})
             .into()
     }
     fn update(&mut self, message: Message) {}
@@ -118,48 +143,106 @@ impl Keyboard {
 
         for i in 0..row1_chars.len() {
             let c = row1_chars.chars().nth(i).unwrap();
-            println!("{}",&c);
             row1_set.push(letter_block(c, self.state.get(&c).unwrap().clone()).into())
         }
         for i in 0..row2_chars.len() {
             let c = row2_chars.chars().nth(i).unwrap();
-            println!("{}",&c);
             row2_set.push(letter_block(c, self.state.get(&c).unwrap().clone()).into())
         }
         for i in 0..row3_chars.len() {
             let c = row3_chars.chars().nth(i).unwrap();
-            println!("{}",&c);
             row3_set.push(letter_block(c, self.state.get(&c).unwrap().clone()).into())
         }
-        let row1 = Row::from_vec(row1_set);
-        let row2 = Row::with_children(row2_set);
-        let row3 = Row::with_children(row3_set);
+        let row1 = Row::from_vec(row1_set).spacing(5)
+            .padding(Padding {top: 30.0, right: 0.0, bottom: 0.0, left: 200.0});
+        let row2 = Row::with_children(row2_set).spacing(5)
+        .padding(Padding {top: 10.0, right: 0.0, bottom: 0.0, left: 230.0});
+        let row3 = Row::with_children(row3_set).spacing(5)
+            .padding(Padding {top: 10.0, right: 0.0, bottom: 0.0, left: 290.0});
         column![row1, row2, row3].into()
     }
+    fn update(&mut self, message: Message) {}
+    
+}
+
+struct Title {
+    text: String,
+}
+
+impl Default for Title {
+    fn default() -> Self {
+        Self {text: "Wordle".to_string()}
+    }
+}
+
+impl Title {
+    fn view(&self) -> Element<'_, Message> {
+        container(text(self.text.clone()).size(50)
+            .font(Font {weight: Weight::Bold, ..Font::default()}))
+            .padding(Padding {top: 20.0, right: 0.0, bottom: 0.0, left: 415.0}).into()
+    }
+    fn update(&mut self, result: GameResult) {}
 }
 
 #[derive(Default)]
 struct Layout {
+    title: Title,
     entry_set: EntrySet,
     keyboard: Keyboard,
 }
 
 impl Layout {
     fn view(&self) -> Element<'_, Message> {
-        column![self.entry_set.view(),self.keyboard.view()].into()
+        column![self.title.view(),self.entry_set.view(),self.keyboard.view()].into()
     }
-    fn update(&mut self, message: Message) {}
+    fn grade(&self, word: [char; 5]) -> [(char, Color); 5] {
+        let mut ans = [('A', *GREY); 5];
+        for i in 0..5 {
+            ans[i].0 = word[i];
+            if word[i] == self.entry_set.secret_word.chars().nth(i).unwrap() {
+                ans[i].1 = *GREEN;
+            } else if self.entry_set.secret_word.contains(word[i]) {
+                ans[i].1 = *YELLOW;
+            } else {
+                ans[i].1 = *DARK_GREY;
+            }
+        }
+        ans
+    }
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::EnterText(c) => {self.entry_set.update(message);},
+            Message::DeleteText => {self.entry_set.update(message);},
+            Message::Enter => {
+                let grade_result = self.grade(self.entry_set.entries[self.entry_set.active_entry].chars);
+                self.entry_set.entries[self.entry_set.active_entry].grade(grade_result);
+                self.keyboard.grade(grade_result);},
+            Message::GameOver(result) => {self.title.update(result);},
+        }
+    }
+    fn subscription(&self) -> Subscription<Message> {
+        keyboard::on_key_press(parse_keyboard_input)
+    }
 }
 
 fn letter_block(c: char, color: Color) -> Container<'static, Message> {
     container(text(c.to_ascii_uppercase().to_string()).size(50))
-        //.padding(1)
         .center(55)
         .style(move |theme| {let mut x = container::rounded_box(theme); x.background = Some(color.into()); x})
         .into()
 }
 
+fn parse_keyboard_input(key: Key, _modifiers: Modifiers) -> Option<Message> {
+    match key {
+        Key::Character(c) => Some(Message::EnterText(c.to_ascii_uppercase().parse().unwrap())),
+        Key::Named(Named::Backspace) => Some(Message::DeleteText),
+        Key::Named(Named::Enter) => Some(Message::Enter),
+        _ => None
+    }
+}
+
 fn main() -> iced::Result {
     //let entry = Entry::from_strings(['h','e','l','l','o']);
-    iced::run("Wordle in Rust!", Layout::update, Layout::view)
+    iced::application("Wordle in Rust!", Layout::update, Layout::view)
+        .subscription(Layout::subscription).run()
 }
